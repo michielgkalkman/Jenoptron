556214f4e922ee44cde9b44e9e59fbdc783b3602
Michiel Kalkman <m>
2015-02-15 20:02:41 +0100

    Getting the CheckboxEditor to work

--- JenoptronFx/src/main/java/dt/fx/CheckboxEditor.java
+++ JenoptronFx/src/main/java/dt/fx/CheckboxEditor.java
@@ -1,5 +1,7 @@
 package dt.fx;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.CheckBox;
 
 import org.controlsfx.control.spreadsheet.SpreadsheetCellEditor;
@@ -7,6 +9,14 @@ import org.controlsfx.control.spreadsheet.SpreadsheetView;
 
 public class CheckboxEditor extends SpreadsheetCellEditor {
 	private final CheckBox checkBox;
+    private ChangeListener<Boolean> cl;
+    /**
+     * This is needed because "endEdit" will call our "end" method too late
+     * when pressing enter, so several "endEdit" will be called. So this
+     * prevent that to happen.
+     */
+    private final boolean ending = false;
+
 	
 	public CheckboxEditor(final SpreadsheetView spreadsheetView) {
 		super(spreadsheetView);
@@ -14,10 +24,15 @@ public class CheckboxEditor extends SpreadsheetCellEditor {
 	}
 
 	@Override
-	public void startEdit(final Object item) {
-		// TODO Auto-generated method stub
+	public void startEdit(final Object value) {
 
-	}
+        if (value instanceof Boolean || value == null) {
+        	checkBox.setSelected((boolean)value);
+        }
+        attachEnterEscapeEventHandler();
+
+        checkBox.requestFocus();
+    }
 
 	@Override
 	public CheckBox getEditor() {
@@ -26,14 +41,50 @@ public class CheckboxEditor extends SpreadsheetCellEditor {
 
 	@Override
 	public String getControlValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return Boolean.toString(checkBox.isSelected());
 	}
 
 	@Override
 	public void end() {
-		// TODO Auto-generated method stub
+        checkBox.setOnKeyPressed(null);
 
 	}
 
+
+    /***************************************************************************
+     * * Private Methods * *
+     **************************************************************************/
+
+    private void attachEnterEscapeEventHandler() {
+        /**
+         * We need to add an EventFilter because otherwise the DatePicker
+         * will block "escape" and "enter". But when "enter" is hit, we need
+         * to runLater the commit because the value has not yet hit the
+         * DatePicker itself.
+         */
+//        eh = new EventHandler<KeyEvent>() {
+//            @Override
+//            public void handle(KeyEvent t) {
+//                if (t.getCode() == KeyCode.ENTER) {
+//                    ending = true;
+//                    endEdit(true);
+//                    ending = false;
+//                } else if (t.getCode() == KeyCode.ESCAPE) {
+//                    endEdit(false);
+//                }
+//            }
+//        };
+//
+//        datePicker.addEventFilter(KeyEvent.KEY_PRESSED, eh);
+
+        cl = new ChangeListener<Boolean>() {
+            @Override
+            public void changed(final ObservableValue<? extends Boolean> arg0, final Boolean arg1, final Boolean arg2) {
+                if (!ending)
+                    endEdit(true);
+            }
+        };
+        
+        checkBox.selectedProperty().addListener(cl);
+    }
 }
