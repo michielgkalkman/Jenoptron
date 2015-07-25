1bf0b09238037624ee337963e6b6221b18b91161
Michiel G. Kalkman <m>
2015-07-25 07:17:55 +0200

    Make image from text

--- JenoptronFx/src/main/java/dt/fx/dtcanvas/DTCanvasPane.java
+++ JenoptronFx/src/main/java/dt/fx/dtcanvas/DTCanvasPane.java
@@ -1,8 +1,10 @@
 package dt.fx.dtcanvas;
 
 import javafx.geometry.VPos;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
@@ -74,21 +76,79 @@ public class DTCanvasPane extends Pane {
 		final GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.clearRect(0, 0, canvas_w, h);
 
+		// final WritableImage writableImage = new WritableImage(40, 40);
+		// writableImage.getPixelWriter().setColor(x, y, c);
+
+		final Canvas canvas2 = new Canvas();
+		canvas2.setWidth(40);
+		canvas2.setHeight(40);
+
+		{
+			final GraphicsContext graphicsContext2D = canvas2.getGraphicsContext2D();
+			graphicsContext2D.setFont(Font.font(font.getFamily(), 40));
+
+			graphicsContext2D.setTextAlign(TextAlignment.CENTER);
+			graphicsContext2D.setTextBaseline(VPos.CENTER);
+
+			graphicsContext2D.clearRect(0, 0, 40, 40);
+
+			graphicsContext2D.fillText("Q", 20, 20, 40);
+		}
+
+		final SnapshotParameters parameters = new SnapshotParameters();
+		parameters.setFill(Color.TRANSPARENT);
+		final WritableImage writableImage = new WritableImage(40, 40);
+		final WritableImage snapshot = canvas2.snapshot(parameters, writableImage);
+
+		gc.drawImage(snapshot, 0, 0);
+
+		// StackPane stackPane = new StackPane();
+		// stackPane.getChildren().addAll(canvas2);
+		// stackPane.snapshot(params, image)
+
+		// http://stackoverflow.com/questions/27912628/how-to-overlap-buttons-text-over-an-image-with-javafx-8
+		// http://docs.oracle.com/javafx/2/image_ops/jfxpub-image_ops.htm
+
+		// http://stackoverflow.com/questions/12991397/placing-javafx-text-over-an-image
+
+		// Place your ImageView in a Stackpane, and on top of the view a Group
+		// containing your Text with the according X/Y coordinates set.
+		// shareimprove this answer
+		//
+		// answered Oct 20 '12 at 19:09
+		// sarcan
+		// 2,014817
+		//
+		//
+		//
+		// If I want to save that image to .png after, what would I have to do?
+		// – julbaril Oct 20 '12 at 21:07
+		//
+		//
+		// Well, you could take a snapshot of the stackpane using
+		// ´Node#snapshot(...).
+		// This will give you a WritableImage, which you can convert to a
+		// BufferedImage instance
+		// using SwingFXUtils.fromFXImage(...), and then write that to disk via
+		// ImageIO`. – sarcan Oct 20 '12 at 21:22
+
 		// gc.setFill(Paint.valueOf(Color.RED.toString()));
 		// gc.fillText("X", canvas_w / 2, (getHeight()) / 2, canvas_w);
 
 		final int fontHeight = 40;
+		gc.setFont(Font.font(font.getFamily(), fontHeight));
+
 		final int y_offset = drawConditions(gc, fontHeight, canvas_w);
 		drawActions(gc, fontHeight, y_offset, canvas_w);
 	}
 
-	private void drawActions(final GraphicsContext gc, final int fontHeight, final int y_offset_, final int canvas_w) {
-		gc.setFont(Font.font(font.getFamily(), fontHeight));
-
+	private void drawActions(final GraphicsContext gc, final int fontHeight, int y_offset_, final int canvas_w) {
 		final double width = 30; // text.getBoundsInLocal().getWidth();
 		final double widthMiddle = width / 2;
 
-		int y_offset = y_offset_;
+		final int widthDescription = 300;
+		final int fontHeightMiddle = fontHeight / 2;
+
 		int y = 0;
 		for (y = 0; y < iDecisionTable.getActions().size(); y++) {
 			final IAction iAction = iDecisionTable.getActions().get(y);
@@ -98,16 +158,15 @@ public class DTCanvasPane extends Pane {
 			gc.setTextBaseline(VPos.CENTER);
 			gc.setFill(Paint.valueOf(Color.WHITE.toString()));
 
-			final int widthDescription = 300;
-			final int fontHeightMiddle = fontHeight / 2;
-			gc.fillText(shortDescription, 0, y_offset_ + y * fontHeight + fontHeightMiddle, widthDescription);
+			gc.fillText(shortDescription, 0, y_offset_ + fontHeightMiddle, widthDescription);
 
-			y_offset += fontHeight;
+			gc.setTextAlign(TextAlignment.CENTER);
+			gc.setTextBaseline(VPos.CENTER);
 
-			int counter = 0;
+			double start_w = widthDescription;
+
 			for (int i = 0; i < iDecisionTable.getRules().size(); i++) {
 
-				final double start_w = widthDescription + counter * width;
 				if (start_w < canvas_w) {
 					final IRule rule = iDecisionTable.getRule(i);
 					final BinaryActionValue actionValue = (BinaryActionValue) rule.getActionValue(iAction);
@@ -136,25 +195,16 @@ public class DTCanvasPane extends Pane {
 
 					gc.setFill(p);
 
-					final javafx.scene.text.Text text = new javafx.scene.text.Text(text2);
-
-					text.setFill(p);
-
-					gc.setTextAlign(TextAlignment.CENTER);
-					gc.setTextBaseline(VPos.CENTER);
-
-					gc.fillText(text2, start_w + widthMiddle, y_offset_ + y * fontHeight + fontHeightMiddle, width);
-
-					counter++;
+					gc.fillText(text2, start_w + widthMiddle, y_offset_ + fontHeightMiddle, width);
 				}
+				start_w += width;
 			}
+			y_offset_ += fontHeight;
 		}
 	}
 
 	private int drawConditions(final GraphicsContext gc, final int fontHeight, final int canvas_w) {
-		gc.setFont(Font.font(font.getFamily(), fontHeight));
-
-		int y_offset = 0;
+		int start_h = 0;
 		int y = 0;
 		final double width = 30; // text.getBoundsInLocal().getWidth();
 		final double widthMiddle = width / 2;
@@ -168,7 +218,7 @@ public class DTCanvasPane extends Pane {
 			gc.setTextBaseline(VPos.CENTER);
 			gc.setFill(Paint.valueOf(Color.WHITE.toString()));
 
-			gc.fillText(shortDescription, 0, y * fontHeight + fontHeightMiddle, widthDescription);
+			gc.fillText(shortDescription, 0, start_h + fontHeightMiddle, widthDescription);
 
 			double start_w = widthDescription;
 
@@ -204,13 +254,13 @@ public class DTCanvasPane extends Pane {
 
 					gc.setFill(p);
 
-					gc.fillText(text2, start_w + widthMiddle, y_offset + fontHeightMiddle, width);
+					gc.fillText(text2, start_w + widthMiddle, start_h + fontHeightMiddle, width);
 				}
 				start_w += width;
 			}
-			y_offset += fontHeight;
+			start_h += fontHeight;
 		}
 
-		return y_offset;
+		return start_h;
 	}
 }
