1d2d82351c08bf722c8726dc4a8ecdbcf4db65eb
Michiel G. Kalkman <m>
2015-07-30 20:37:02 +0200

    Going for DTView

---
+++ JenoptronFx/src/main/java/dt/dtview/Column.java
@@ -0,0 +1,15 @@
+package dt.dtview;
+
+public class Column {
+
+	private final int width;
+
+	public Column(final int width) {
+		this.width = width;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+}
